98ef1a3bec2b4e466352aa0d6f5c621de69f9aec
Finist <finistars@mail.ru>
2022-12-21 23:09:29 +0300

    clean init project

--- postgres/scripts/db-v2.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE payments;
-GRANT ALL PRIVILEGES ON DATABASE payments TO program;
-
-CREATE DATABASE reservations;
-GRANT ALL PRIVILEGES ON DATABASE reservations TO program;
-
-CREATE DATABASE loyalties;
-GRANT ALL PRIVILEGES ON DATABASE loyalties TO program;
